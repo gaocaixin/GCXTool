b485d93c5bf491c78832c3488d0be479024da841
高才新 <767374741@qq.com>
2015-12-07 18:39:27 +0800

    1

---
+++ GCXTool/GCXToolUI.h
@@ -0,0 +1,15 @@
+//
+//  GCXToolUI.h
+//  IOS_TOOL
+//
+//  Created by 高才新 on 15/12/7.
+//  Copyright © 2015年 高才新. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface GCXToolUI : NSObject
+
+- (UIButton *)buttonWitnImage:(UIImage *)image;
+
+@end
---
+++ GCXTool/GCXToolUI.m
@@ -0,0 +1,13 @@
+//
+//  GCXToolUI.m
+//  IOS_TOOL
+//
+//  Created by 高才新 on 15/12/7.
+//  Copyright © 2015年 高才新. All rights reserved.
+//
+
+#import "GCXToolUI.h"
+
+@implementation GCXToolUI
+
+@end
